e9934ad27310518d93ef44ec2fd4a9dbdd58e599
Abhishek Dhapare <abhishek.dhapare@gmail.com>
2022-03-28 14:43:13 +0530

    Events and events detailed page created

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,8 +1,36 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
+import { getEventById } from "../../dummy-data";
+
 const EventDetails = () => {
+  const router = useRouter();
+
+  const eventId = router.query.id;
+
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>Event not found.</p>;
+  }
+
   return (
-    <div>
-      <h1>Event Details Page (Show Selected Event)</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 };
 
